bcbecf2545b753f17de2ba650155ff1cc211eb28
Elaidzha1940 <eli080293@gmail.com>
2024-09-09 04:15:05 +0300

    PhotoGallery

---
+++ PhotoGallery/ContentView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: PhotoGallery
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.09.2024
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: PhotoGallery
+//  File: PhotoGalleryApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.09.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
