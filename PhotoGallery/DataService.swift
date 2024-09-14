6d5f8525f78301e2cec529b413bb61abd120f5df
Elaidzha1940 <eli080293@gmail.com>
2024-09-14 12:19:08 +0300

    Update DataService

--- PhotoGallery/DataService.swift
+++ PhotoGallery/DataService.swift
@@ -8,3 +8,19 @@
 //  */
 
 import Foundation
+
+class ImageSelection: ObservableObject {
+    @Published var selectedData: dataMode? = nil
+    @Published var showingSheet: Bool = false
+}
+
+struct dataMode: Identifiable, Equatable {
+    var id = UUID()
+    var name: String
+    var title: String
+    
+    static func == (lhs: dataMode, rhs: dataMode) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
