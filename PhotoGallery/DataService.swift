1d10a8d6a28631fb6cd7cad9996b50b040aaa357
Elaidzha1940 <eli080293@gmail.com>
2024-09-15 13:04:01 +0300

    Debug DataService

--- PhotoGallery/DataService.swift
+++ PhotoGallery/DataService.swift
@@ -25,46 +25,43 @@ struct dataMode: Identifiable, Equatable {
 }
 
 var theImages = [
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey")
 ]
 
 var theImages1 = [
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey")
 ]
 
 var theImages2 = [
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    dataMode(id: "Hey", name: "Hey", title: "Hey"),
-    
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey"),
+    dataMode(name: "Hey", title: "Hey")
 ]
 
-struct sideBar: Identifiable {
+struct sideBars: Identifiable {
     var id = UUID()
     var icon: String
     var title: String
-    var tab: TabIcon1
+    var tab: TabIcon
     var insdex: Int
 }
 
-let sideBar = [
-    sideBar(icon: "square.stack", title: "Home", tab: .Card, insdex: 0),
-    sideBar(icon: "square.stack", title: "Card", tab: .Card, insdex: 1),
-    sideBar(icon: "square.stack", title: "Purchases", tab: .Card, insdex: 2),
-    sideBar(icon: "square.stack", title: "Favorite", tab: .Card, insdex: 3),
-    sideBar(icon: "square.stack", title: "Notification", tab: .Card, insdex: 4),
+let sideBars = [
+    sideBars(icon: "square.stack", title: "Favorite", tab: .Favorite, insdex: 0),
+    sideBars(icon: "magnifyingglass", title: "Home", tab: .Home, insdex: 1),
+    sideBars(icon: "house", title: "Card", tab: .Card, insdex: 2),
+    sideBars(icon: "star", title: "Purchases", tab: .Purchases, insdex: 3),
+    sideBars(icon: "person", title: "Notification", tab: .Notification, insdex: 4),
 ]
 
 enum TabIcon: String {
@@ -75,7 +72,7 @@ enum TabIcon: String {
     case Notification
 }
 
-struct sideBar1: Identifiable {
+struct sideBars1: Identifiable {
     var id = UUID()
     var icon: String
     var title: String
@@ -83,12 +80,12 @@ struct sideBar1: Identifiable {
     var insdex: Int
 }
 
-let sideBar1 = [
-    sideBar(icon: "square.stack", title: "Home", tab: .Card, insdex: 0),
-    sideBar(icon: "square.stack", title: "Card", tab: .Card, insdex: 1),
-    sideBar(icon: "square.stack", title: "Purchases", tab: .Card, insdex: 2),
-    sideBar(icon: "square.stack", title: "Favorite", tab: .Card, insdex: 3),
-    sideBar(icon: "square.stack", title: "Notification", tab: .Card, insdex: 4),
+let sideBars1 = [
+    sideBars1(icon: "house.fill", title: "Home", tab: .Home, insdex: 0),
+    sideBars1(icon: "creditcard.fill", title: "Notification", tab: .Notification, insdex: 1),
+    sideBars1(icon: "heart.fill", title: "Favorite", tab: .Favorite, insdex: 2),
+    sideBars1(icon: "cart.fill.badge.plus", title: "Purchases", tab: .Purchases, insdex: 3),
+    sideBars1(icon: "bell.badge.fill", title: "Favorite", tab: .Favorite, insdex: 4),
 ]
 
 enum TabIcon1: String {
